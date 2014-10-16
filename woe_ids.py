2c69c387ad90a2fe86448deb9b173e1b10b45b3b
Aitor Almeida <aitoralmeida@gmail.com>
2014-10-16 15:46:51 +0200

    woe ids for several countries and areas

---
+++ woe_ids.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 29 09:48:14 2014
+
+@author: aitor
+"""
+
+continents = {
+    'Africa' : 24865670,
+    'Europe' : 24865675,
+    'South America' : 24865673,
+    'Antarctic' : 28289421,
+    'Asia' : 24865671,
+    'North America' : 24865672,
+    'Australia' : 55949070,
+}
+
+countries = {
+    'Russia' : 23424936,
+    'Poland' : 23424923,
+    'Ukraine' : 23424976,
+    'United States' : 23424977,
+    'Spain' : 23424950,
+    'Germany' : 23424829,
+
+}
