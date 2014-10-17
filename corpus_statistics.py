4d87e263d482b4e48b3e50e48c06f9e3ed8fcd0c
Aitor Almeida <aitoralmeida@gmail.com>
2014-10-17 11:15:53 +0200

    Methods to calculate basic corpus statistics

---
+++ corpus_statistics.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 17 11:07:36 2014
+
+@author: aitor
+"""
+from glob import glob
+import json
+
+CORPUS = 'corpus' #corpus, corpus_lite
+
+def total_users():
+    users = set()
+    total_retweets = 0
+    total_tweets = 0
+    for i, file_name in enumerate(glob('./' + CORPUS + '/*.txt')):
+        print 'Processing %i of %i files' % (i, len(glob('./' + CORPUS + '/*.txt')))
+        with open(file_name, 'r') as f:
+            for line in f:
+                try:
+                    tweet = json.loads(line)
+                except:
+                    continue
+            
+                try:
+                    user_id = tweet['user']['id']
+                    users.add(user_id)
+                except:
+                    continue
+                
+                total_tweets += 1
+                
+                if tweet.has_key('retweeted_status'):
+                    total_retweets += 1
+    
+    print '\nRESULTS:'                
+    print ' - Total users:', len(users)
+    print ' - Total tweets:', total_tweets
+    print ' - Total retweets:', total_retweets
+    
+if __name__=='__main__':  
+    print 'Starting...'
+    
+    total_users()
+    
+    print 'DONE'
+    
