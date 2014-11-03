aa7808e80f008a418443c2d2ca2f5e3258e7ce72
Aitor Almeida <aitoralmeida@gmail.com>
2014-11-03 16:10:30 +0100

    added percentages

--- corpus_statistics.py
+++ corpus_statistics.py
@@ -16,6 +16,7 @@ def total_users():
     total_retweets = 0
     total_tweets = 0
     total_mentions = 0
+    total_replies = 0
     tweet_times = {} # {'1230' : 10, '1200': 4}
     for i, file_name in enumerate(glob('./' + CORPUS + '/*.txt.gz')):
         print 'Processing %i of %i files' % (i, len(glob('./' + CORPUS + '/*.txt.gz')))
@@ -39,6 +40,10 @@ def total_users():
                     
                 total_mentions += len([mention['id_str'] for mention in tweet['entities']['user_mentions']])
                 
+                if tweet['in_reply_to_user_id']:                
+                    total_replies += 1
+                    
+                
                 post_datetime = tweet['created_at']
                 time = post_datetime.split(' ')[3]
                 hour = time.split(':')[0]
@@ -58,9 +63,10 @@ def total_users():
     print '\nRESULTS:'                
     print ' - Total users:', len(users)
     print ' - Total tweets:', total_tweets
-    print ' - Original tweets:', total_tweets - total_retweets
-    print ' - Total retweets:', total_retweets
-    print ' - Total mentions:', total_mentions
+    print ' - Original tweets: %s(%s%%)' % (total_tweets - total_retweets, ((total_tweets - total_retweets) * 1.0 / total_tweets) * 100.0)
+    print ' - Total replies: %s(%s%%)' % (total_replies, (total_replies * 1.0 / total_tweets) * 100.0)
+    print ' - Total retweets: %s(%s%%)' % (total_retweets, (total_retweets * 1.0 / total_tweets) * 100.0)
+    print ' - Total mentions: %s' % (total_mentions)
     print tweet_times
     tweet_series = Series(tweet_times)
     tweet_series.plot()
