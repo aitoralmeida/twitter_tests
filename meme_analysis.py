5c971fcd7d881092e5160298dd716b443dd87d85
Aitor Almeida <aitoralmeida@gmail.com>
2014-11-05 12:38:29 +0100

    added method to calculate source diversity (#7)

--- meme_analysis.py
+++ meme_analysis.py
@@ -14,8 +14,8 @@ import gzip
 CORPUS = 'corpus' #corpus, corpus_lite
 SEED = ['p2', 'tcot', 'gov', 'dem', 'dems', 'gop']
 
-def count_meme_appearance():    
-    meme_days = {} #{'meme' : {'month' : {'day' : 14}}}
+def count_meme_appearances():    
+    meme_days = {} #{'meme' : {'month' : {'day' : {'id' : 14, 'id2' : 1}}}
     for i, file_name in enumerate(glob('./' + CORPUS + '/*.txt.gz')):
         print 'Processing %i of %i files' % (i, len(glob('./' + CORPUS + '/*.txt.gz')))
         with gzip.open(file_name, 'r') as f:
@@ -53,6 +53,37 @@ def count_meme_appearance():
                         
     json.dump(meme_days, open('meme_count.json', 'w'), indent=2)
     
+def count_meme_id_diversity():
+    meme_count = json.load(open('meme_count.json', 'r'))
+    meme_diversity = {}
+    for i, meme in enumerate(meme_count):     
+        if i % 100 == 0:
+            print 'Processing %s of %s' % (i, len(meme_count))    
+            
+        total_total_ids = []
+                
+        for month in meme_count[meme]:
+            for day in meme_count[meme][month]:
+                total_ids = [id_user for id_user in day if id_user != 'total']
+                different_ids = set(total_ids)
+                total_total_ids = total_total_ids + total_ids
+                
+                diversity = (len(different_ids) * 100.0) / len(total_ids)
+                if not meme_diversity.has_key(meme):
+                    meme_diversity[meme] = {month : {day : diversity}}
+                elif not meme_diversity[meme].has_key(month):
+                    meme_diversity[meme][month] = {day : diversity}
+                elif not meme_diversity[meme][month].has_key(day):          
+                    meme_diversity[meme][month][day] = diversity
+                    
+        total_different_ids = set(total_total_ids)   
+        meme_diversity[meme]['total_diversity'] = (len(total_different_ids) * 100.0) / len(total_total_ids)        
+    json.dump(meme_diversity, open('meme_source_diversity.json', 'w'), indent=2)
+                
+                        
+    
+
+    
 def _add_meme(meme_days, meme, month, day, user_id):       
     if not meme_days.has_key(meme):
         meme_days[meme] = {month : {day : {'total' : 1, user_id : 1}}}
@@ -77,6 +108,6 @@ def _add_meme(meme_days, meme, month, day, user_id):
 if __name__=='__main__':  
     print 'Starting...'
     
-    count_meme_appearance()
-    
+    #count_meme_appearances()
+    count_meme_id_diversity()
     print 'DONE'
