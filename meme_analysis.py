b19e8c41b90f9c3e8c5a0ddc5a8c40cb21b57a99
Aitor Almeida <aitoralmeida@gmail.com>
2014-11-07 15:13:38 +0100

    added meme origin to meme_count.json (#7)

--- meme_analysis.py
+++ meme_analysis.py
@@ -17,9 +17,11 @@ DATA = 'data' #data output folder
 SEED = ['p2', 'tcot', 'gov', 'dem', 'dems', 'gop']
 FILTER_QUANTILE = 0.95 # we only use the 5% most relevant memes
 
-def count_meme_appearances():  
-    print 'Counting meme appearances'
+def count_meme_appearances(): 
     # Counts how many times a meme appears and which are the injection points
+    print 'Counting meme appearances'    
+    total_tags = 0
+    total_urls = 0
     meme_days = {} #{'meme' : {'month' : {'day' : {'id' : 14, 'id2' : 1}}}
     for i, file_name in enumerate(glob('./' + CORPUS + '/*.txt.gz')):
         if i % 10 == 0:
@@ -40,25 +42,35 @@ def count_meme_appearances():
                 except:
                     continue
                 
+                from_id = ''
+                if tweet.has_key('retweeted_status'):
+                    from_id = tweet['retweeted_status']['user']['id']  
+                    print user_id
+                
                 try:
                     tags = [x['text'] for x in tweet['entities']['hashtags']]
                     for t in tags:
                         tag = t.lower()
                         if not tag in SEED:                                             
-                            _add_meme(meme_days, tag, month, day, user_id)                    
+                            _add_meme(meme_days, tag, month, day, user_id, from_id)
+                            total_tags += 1
                 except:
                     pass
                 
                 try:
                     urls = [m['expanded_url'] for m in tweet['entities']['urls']]
                     for url in urls:
-                        _add_meme(meme_days, url, month, day, user_id)   
+                        _add_meme(meme_days, url, month, day, user_id, from_id) 
+                        total_urls += 1
                 except:
                     pass
+    print 'Total tags: %s' % (total_tags)
+    print 'Total URLs: %s' % (total_urls)
     print 'Writing file...'                    
     json.dump(meme_days, open('./' + DATA + '/meme_count.json', 'w'), indent=2)
     
 def count_meme_id_diversity():
+    # calculates the diversity of each meme, both per day and global.
     print 'Calculating meme diversity'
     print ' - Loading file'
     meme_count = json.load(open('./' + DATA + '/meme_count.json', 'r'))
@@ -76,7 +88,7 @@ def count_meme_id_diversity():
                 different_ids = set ()
                 for id_user in meme_count[meme][month][day]:
                     if id_user != 'total':
-                        total_msgs += meme_count[meme][month][day][id_user] 
+                        total_msgs += meme_count[meme][month][day][id_user]['total'] 
                         different_ids.add(id_user)
                         total_different_ids.add(id_user)
 
@@ -96,13 +108,14 @@ def count_meme_id_diversity():
     print 'Writing file...'   
     json.dump(meme_diversity, open('./' + DATA + '/meme_source_diversity.json', 'w'), indent=2)
     
-def _add_meme(meme_days, meme, month, day, user_id):       
+def _add_meme(meme_days, meme, month, day, user_id, from_id):
+    # Adds a meme to the dict       
     if not meme_days.has_key(meme):
-        meme_days[meme] = {month : {day : {'total' : 1, user_id : 1}}}
+        meme_days[meme] = {month : {day : {'total' : 1, user_id : {'total' : 1, 'from_id': [from_id]}}}}
     elif not meme_days[meme].has_key(month):
-        meme_days[meme][month] = {day : {'total' : 1, user_id : 1}}
+        meme_days[meme][month] = {day : {'total' : 1, user_id : {'total' : 1, 'from_id': [from_id]}}}
     elif not meme_days[meme][month].has_key(day):          
-        meme_days[meme][month][day] = {'total' : 1, user_id : 1}
+        meme_days[meme][month][day] = {'total' : 1, user_id : {'total' : 1, 'from_id': [from_id]}}
     elif not meme_days[meme][month][day].has_key(user_id):
         try:
             total = meme_days[meme][month][day]['total']
@@ -110,21 +123,32 @@ def _add_meme(meme_days, meme, month, day, user_id):
         except:
             total = 1  
         meme_days[meme][month][day]['total'] = total
-        meme_days[meme][month][day][user_id] = 1
+        meme_days[meme][month][day][user_id]['total'] = 1
+        meme_days[meme][month][day][user_id]['from_id'] = [from_id]
 
     else:  
         meme_days[meme][month][day]['total'] += 1
-        meme_days[meme][month][day][user_id] += 1
+        meme_days[meme][month][day][user_id]['total'] += 1
+        unique_ids = set(meme_days[meme][month][day][user_id]['from_id'])
+        unique_ids.add(from_id)
+        meme_days[meme][month][day][user_id]['from_id'] = list(unique_ids)
+        meme_days[meme][month][day][user_id]['from_id'].add(from_id)
         
 def filter_relevant_memes():
+    # filters relevant memes acording to the quantile specified in FILTER_QUANTILE
     print 'Filtering relevant memes'
     print ' - Loading file...' 
     meme_diversity = json.load(open('./' + DATA + '/meme_source_diversity.json', 'r'))
-    msg_totals = [meme_diversity[meme]['total_msgs'] for meme in meme_diversity]
     
+    # get the msg number for each meme
+    msg_totals = [meme_diversity[meme]['total_msgs'] for meme in meme_diversity]
+    # calculate the minumun msg ammount necessary to be relevant. We do this taking
+    # the higher 5%.
     totals = pd.Series(msg_totals)
-    min_msgs = totals.quantile(FILTER_QUANTILE) # only use the 5% most relevant
+    min_msgs = totals.quantile(FILTER_QUANTILE)
+    # get the relevant memes
     relevant_memes = [meme for meme in meme_diversity if meme_diversity[meme]['total_msgs'] >= min_msgs]
+    # copy their data
     filtered_meme_diversity = {}
     for meme in relevant_memes:
         filtered_meme_diversity[meme] = meme_diversity[meme]
